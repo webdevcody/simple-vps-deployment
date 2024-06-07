e22704f2c2346404a491a337022fd3199de99c77
Cody Seibert <webdevcody@gmail.com>
2024-06-06 21:54:18 -0400

    fixing typescript error

--- src/app/(landing)/_sections/newsletter.tsx
+++ src/app/(landing)/_sections/newsletter.tsx
@@ -69,7 +69,7 @@ export default function Newsletter({
                     d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                   />
                 </svg>
-                <span>You've been subscribed!</span>
+                <span>You&apos;ve been subscribed!</span>
               </div>
             )}
           </div>
